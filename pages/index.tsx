d39a8450d16e214ab7156e7d0b61dbab452dd8cf
espere-papillon <marinabov007@gmail.com>
2021-09-04 22:57:03 +0300

    add description contentful model

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,34 @@
 import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import {client} from "../contentfull";
 
-const Home: NextPage = () => {
+export default function Home ({title, home}: {title: string, home: any}) {
+    console.log(home)
   return (
     <div>
       <Head>
-        <title>Blog</title>
+        <title>{home.fields.title}</title>
       </Head>
 
       <main>
+          <h1>{home.fields.title}</h1>
       </main>
     </div>
   )
 }
 
-export default Home
+export const getStaticProps: GetStaticProps = async () => {
+    const home = await client.getEntries({
+        content_type: 'home',
+        limit: 1
+    })
+
+    const [homePage] = home.items
+    return {
+        props: {
+            title: 'Blog',
+            home: homePage,
+        }
+    }
+}
